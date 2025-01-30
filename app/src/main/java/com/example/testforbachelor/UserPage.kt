04589c68c015a64f6e8ad956a39fa3e8c9596c7c
GamerForFunn <Julian.Skog@Gmail.com>
2025-01-30 15:53:21 +0100

    Messing about with XML and Kotlin

---
+++ app/src/main/java/com/example/testforbachelor/UserPage.kt
@@ -0,0 +1,4 @@
+package com.example.testforbachelor
+
+class UserPage {
+}
--- app/src/main/java/com/example/testforbachelor/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/testforbachelor/ui/dashboard/DashboardViewModel.kt
@@ -5,9 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DashboardViewModel : ViewModel() {
-
+    fun genNum(): Int{
+        return (1..3).random()
+    }
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        val random = genNum()
+        when(random){
+            1 -> value = "Testcase 1"
+            2 -> value = "Testcase 2"
+            3 -> value = "Testcase 3"
+        }
+
+        //value = "This is dashboard Fragment"
+
     }
     val text: LiveData<String> = _text
 }
